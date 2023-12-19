ab1a4c91cd0790ffd6e18799fbd40d39d6c7001a
averine08 <averinanurdin08@gmail.com>
2023-12-19 22:34:21 +0700

    addDaisyUI, add : colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,45 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '0px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '576px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors:{
+        'ungu':{
+          DEFAULT : '#4E6DA9', // gelap navbar
+        },
+        'putih' : {
+          DEFAULT : '#FFFFFF',
+        },
+        'pink' :{
+          DEFAULT: '#ECA3C3', // untuk button
+          dark : '#CF95AF', // untuk teks
+        },
+        'merah' :{
+          DEFAULT : '#EE5F5F',
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  safelist : [
+    /(bg|text|border)-(ungu|putih|pink|merah)/,
+    /(bg|text|border)-(pink)-(DEFAULT|dark)/,
+  ],
 }
+
 
