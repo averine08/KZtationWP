cda3cf368e8da36265f61900fee1599989a59ea9
averine08 <averinanurdin08@gmail.com>
2023-12-20 13:14:33 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::get('/', function () {
     return view('layouts.home');
 });
 
-<<<<<<< HEAD
 Route::view('/login', 'login');
 Route::view('/home', 'home');
 Route::post('/login', [PenggunaAuth::class,'userlogin']);
@@ -77,6 +76,3 @@ Route::delete('/payment-detail', [PaymentDetailController::class, 'delete_all_pa
 Route::get('/payment-detail/{params_id}', [PaymentDetailController::class, 'find_one_paymentdetail']);
 Route::patch('/payment-detail/{params_id}', [PaymentDetailController::class, 'update_one_paymentdetail']);
 Route::delete('/payment-detail/{params_id}', [PaymentDetailController::class, 'delete_one_paymentdetail']);
-=======
-
->>>>>>> dev
